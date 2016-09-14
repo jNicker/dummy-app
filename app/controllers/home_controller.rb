ad910c24e1ac189a670ec7bf033a293ce6257942
Igor Rzegocki <ajgon@irgon.com>
2016-09-14 14:07:47 +0200

    resque

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     data = SecureRandom.hex(32)
     Dummy.create(field: data)
+    DummyJob.perform_later
     @item = Dummy.last.field
   end
 end
