fbb31e546d11136a39113cbee19e39a5c293322c
Igor Rzegocki <ajgon@users.noreply.github.com>
2017-05-29 10:43:46 +0200

    Update after_restart.rb

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,6 +3,6 @@ Chef::Log.info('Running deploy/after_restart.rb...')
 
 execute 'copy assets' do
   cwd release_path
-  command "mkdir -p #{release_path}/public/test && cp -r #{release_path}/public/assets/* #{release_path}/public/test"
+  command "mkdir -p #{release_path}/public/test && cp -r #{release_path}/public/assets/* #{release_path}/public/test || true"
 end
 
